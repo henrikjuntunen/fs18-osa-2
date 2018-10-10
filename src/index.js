dc1f6ef8fa04ef633db82e4e317b883e6fccfa81
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-10 20:50:43 +0300

    Osa2 Tehtävä 2.9

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Puhelinluettelo.2.8'
+import App from './Puhelinluettelo.2.9'
 // Osa2 tehtävä 2.1 App21.js components/Kurssi.21.2.js
 // Osa2 tehtävä 2.2 App21.js components/Kurssi.22.2.js
 // Osa2 tehtävä 2.3 App21.js components/Kurssi.23.2.js
@@ -9,6 +9,7 @@ import App from './Puhelinluettelo.2.8'
 // Osa2 tehtävä 2.6 components/Puhelinluettelo.2.6.js
 // Osa2 tehtävä 2.7 components/Puhelinluettelo.2.7.js
 // Osa2 tehtävä 2.8 components/Puhelinluettelo.2.8.js
+// Osa2 tehtävä 2.9 components/Puhelinluettelo.2.9.js
 ReactDOM.render(
     <App />,
     document.getElementById('root')
