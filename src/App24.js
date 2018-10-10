23fd5325550717fb9185c39dba7a569fadc176e5
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-10 20:23:30 +0300

    Osa2 Tehtävä 2.4

---
+++ src/App24.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import Kurssi from './Kurssi24'
+// Osa2 tehtävä 2.4
+const App24 = () => {
+
+  
+      const kurssi = [
+      {
+        nimi: 'Half Stack -sovelluskehitys',
+        id: 1,
+        osat: [
+          {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10,
+            id: 1
+          },
+          {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7,
+            id: 2
+          },
+          {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14,
+            id: 3
+          },
+          {
+            nimi: 'Reduce perusteet',
+            tehtavia: 5,
+            id: 4
+          }
+        ]
+      },
+      {
+        nimi: 'Node.js',
+        id: 2,
+        osat: [
+          {
+            nimi: 'Routing',
+            tehtavia: 3,
+            id: 1
+          },
+          {
+            nimi: 'Middlewaret',
+            tehtavia: 7,
+            id: 2
+          }
+        ]
+      }
+      ]
+    
+
+  return (
+      <div>
+        <Kurssi kurssiTa={kurssi} />
+      </div>
+    )
+  }
+
+  export default App24
---
+++ src/index.1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App21'
+// Osa2 tehtävä 2.1 App21.js components/Kurssi.21.2js
+// Osa2 tehtävä 2.2 App21.js components/Kurssi.22.2js
+// Osa2 tehtävä 2.3 App21.js components/Kurssi.23.2js
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+    )
