c7facdaaf642a186e3c927f06f1d03f5fcc85178
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-10 20:23:08 +0300

    Osa2 Tehtävä 2.4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App21'
-// Osa2 tehtävä 2.1 App21.js components/Kurssi.21.2js
-// Osa2 tehtävä 2.2 App21.js components/Kurssi.22.2js
-// Osa2 tehtävä 2.3 App21.js components/Kurssi.23.2js
+import App from './App24'
+// Osa2 tehtävä 2.1 App21.js components/Kurssi.21.2.js
+// Osa2 tehtävä 2.2 App21.js components/Kurssi.22.2.js
+// Osa2 tehtävä 2.3 App21.js components/Kurssi.23.2.js
+// Osa2 tehtävä 2.4 App24.js components/Kurssi.24.js
 ReactDOM.render(
     <App />,
     document.getElementById('root')
