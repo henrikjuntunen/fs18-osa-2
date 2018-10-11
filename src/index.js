8525a8a8ae7520fe1373a76a2e65cffd209979f8
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-10-11 20:26:00 +0300

    Osa 2 Tehtävä 2.15

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './Puhelinluettelo.2.14'
+import App from './Puhelinluettelo.2.15'
 // Osa2 tehtävä 2.1 App21.js components/Kurssi.21.2.js
 // Osa2 tehtävä 2.2 App21.js components/Kurssi.22.2.js
 // Osa2 tehtävä 2.3 App21.js components/Kurssi.23.2.js
